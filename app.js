43f30700995a37c768862d955566fe06101b9237
user <user@MacBook-Pro-de-user.local>
2019-07-03 15:23:50 -0300

    added connection to MongoDB

--- app.js
+++ app.js
@@ -2,45 +2,32 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const graphqlHttp = require("express-graphql");
 const { buildSchema } = require("graphql");
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const User = require("./models/user");
 
 const app = express();
 
 app.use(bodyParser.json());
-
-let users = [];
-
-let idCount = 0;
 
 app.use(
   "/graphql",
   graphqlHttp({
     schema: buildSchema(`
     type Query {
-        info: String!
-        feed: [Link!]!
         user: [User!]!
       }
       
       type User {
         _id: ID!
+        name: String!
         email: String!
         password: String
       }
       
-      input UserInput {
-        email: String!
-        password: String!
-      }
-      
       type Mutation {
-        post(url: String!, description: String!): Link!
-        createUser(userInput: UserInput): User
-      }
-      
-      type Link {
-        id: ID!
-        description: String!
-        url: String!
+        createUser(name: String!, email: String!, password: String!): User!
       }
       
       schema {
@@ -49,63 +36,57 @@ app.use(
       }
     `),
     rootValue: {
-      Query: {
-        info: () => {
-          return `This is the API of a Hackernews Clone`;
-        },
-        feed: () => {
-          return "teste";
-        },
-        user: () => {
-          return users;
-        }
+      user: () => {
+        return User.find()
+          .then(users => {
+            return users.map(user => {
+              return { ...user._doc, _id: user.id };
+            });
+          })
+          .catch(err => {
+            throw err;
+          });
       },
 
-      Mutation: {
-        post: (parent, args) => {
-          const link = {
-            id: `link-${idCount++}`,
-            description: args.description,
-            url: args.url
-          };
-          return link;
-        },
-
-        createUser: args => {
-          const user = {
-            _id: Math.random().toString(),
-            email: args.userInput.email,
-            password: args.userInput.password
-          };
-          users.push(user);
-          return user;
-        }
-        // createUser: args => {
-        //   return User.findOne({ email: args.userInput.email })
-        //     .then(user => {
-        //       if (user) {
-        //         throw new Error("User already existis.");
-        //       }
-        //       return bcrypt.hash(args.userInput.password, 12);
-        //     })
-        //     .then(hashedPassword => {
-        //       const user = new User({
-        //         email: args.userInput.email,
-        //         password: hashedPassword
-        //       });
-        //       return user.save();
-        //     })
-        //     .then(result => {
-        //       return { ...result._doc, password: null, _id: result.id };
-        //     })
-        //     .catch(err => {
-        //       throw err;
-        //     });
-        // }
+      createUser: args => {
+        return User.findOne({ email: args.email })
+          .then(user => {
+            if (user) {
+              throw new Error("User already existis.");
+            }
+            return bcrypt.hash(args.password, 12);
+          })
+          .then(hashedPassword => {
+            const user = new User({
+              name: args.name,
+              email: args.email,
+              password: hashedPassword
+            });
+            return user.save();
+          })
+          .then(result => {
+            return { ...result._doc, password: null, _id: result.id };
+          })
+          .catch(err => {
+            throw err;
+          });
       }
     },
     graphiql: true
   })
 );
 
-app.listen(3000);
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${
+      process.env.MONGO_PASSWORD
+    }@cluster0-sayzk.mongodb.net/${
+      process.env.MONGO_DB
+    }?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
